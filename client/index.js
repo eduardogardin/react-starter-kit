d32aafb77b4fbee5790c7f08930975ee431c1759
Eduardo Gardin <eduardoj.gardin@gmail.com>
2017-02-02 21:47:49 -0200

    Added build script for bundle app in prod mode, added required dependencies, etc

--- client/index.js
+++ client/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
+import 'index.html'
+
 ReactDOM.render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,130 @@
-/**
- * webpack main config 
- */
+const webpack = require('webpack');
+const path = require('path');
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './src/index.html',
-  filename: 'index.html',
-  inject: 'body'
-});
+const sourcePath = path.join(__dirname, './client');
+const distPath = path.join(__dirname, './dist');
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: './dist',
-        filename: 'index_bundle.js'
+module.exports = function (env) {
+  const nodeEnv = env && env.prod ? 'production' : 'development';
+  const isProd = nodeEnv === 'production';
+
+  const plugins = [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: Infinity,
+      filename: 'vendor.bundle.js'
+    }),
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: JSON.stringify(nodeEnv) }
+    }),
+    new webpack.NamedModulesPlugin(),
+  ];
+
+  if (isProd) {
+    plugins.push(
+      new webpack.LoaderOptionsPlugin({
+        minimize: true,
+        debug: false
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false,
+          screw_ie8: true,
+          conditionals: true,
+          unused: true,
+          comparisons: true,
+          sequences: true,
+          dead_code: true,
+          evaluate: true,
+          if_return: true,
+          join_vars: true,
+        },
+        output: {
+          comments: false,
+        },
+      })
+    );
+  } else {
+    plugins.push(
+      new webpack.HotModuleReplacementPlugin()
+    );
+  }
+
+  return {
+    devtool: isProd ? 'source-map' : 'eval',
+    context: sourcePath,
+    entry: {
+      js: './index.js',
+      vendor: ['react']
     },
-    externals: {
-        'cheerio': 'window',
-        'react/lib/ExecutionEnvironment': true,
-        'react/lib/ReactContext': true,
+    output: {
+      path: distPath,
+      filename: '[name].bundle.js',
     },
     module: {
-        rules: [
-            {
-                test: /(\.js$|\.jsx?$)/ ,
-                loader: "babel-loader",
-                exclude: /(node_modules|bower_components)/
-            }            
-        ]       
+      rules: [
+        {
+          test: /\.html$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'file-loader',
+            query: {
+              name: '[name].[ext]'
+            },
+          },
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: [
+            'babel-loader'
+          ],
+        },
+      ],
     },
-    plugins: [HtmlWebpackPluginConfig]
-}
+    resolve: {
+      extensions: ['.webpack-loader.js', '.web-loader.js', '.loader.js', '.js', '.jsx'],
+      modules: [
+        path.resolve(__dirname, 'node_modules'),
+        sourcePath
+      ]
+    },
+
+    plugins,
+
+    performance: isProd && {
+      maxAssetSize: 200,
+      maxEntrypointSize: 300,
+      hints: 'warning',
+    },
+
+    stats: {
+      colors: {
+        green: '\u001b[32m',
+      }
+    },
+
+    devServer: {
+      contentBase: './client',
+      historyApiFallback: true,
+      port: 3000,
+      compress: isProd,
+      inline: !isProd,
+      hot: !isProd,
+      stats: {
+        assets: true,
+        children: false,
+        chunks: false,
+        hash: false,
+        modules: false,
+        publicPath: false,
+        timings: true,
+        version: false,
+        warnings: true,
+        colors: {
+          green: '\u001b[32m',
+        }
+      },
+    }
+  };
+};
