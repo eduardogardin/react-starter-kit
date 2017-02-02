9f54dfa5cda0a433e66120e2a23d541e9f1bb5a4
Eduardo Gardin <eduardoj.gardin@gmail.com>
2017-02-01 23:22:47 -0200

    Initial commit rendering example component

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class App extends React.Component {
+
+    render() {
+        return (
+            <div style={{textAlign: 'center'}}>
+                <h1>Hello React Modern Fucking Seed :)
+                </h1>
+            </div>
+        );
+    }
+}
